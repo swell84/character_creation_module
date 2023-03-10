57a83b36c5ac154a102de3f5f40951b7ecdebe31
Марина <aprel80@inbox.ru>
2023-03-10 12:32:29 +0300

    mod_main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from random import randint
-from graphic_arts.start_game_banner import run
 
 
 def attack(char_name: str, char_class: str) -> str:
@@ -77,16 +76,3 @@ def choice_char_class() -> str:
                                'или любую другую кнопку, '
                                'чтобы выбрать другого персонажа ').lower()
     return char_class
-
-
-if __name__ == '__main__':
-    run_screensaver()
-    print('Приветствую тебя, искатель приключений!')
-    print('Прежде чем начать игру...')
-    char_name: str = input('...назови себя: ')
-    print(f'Здравствуй, {char_name}! '
-          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
-    print('Ты можешь выбрать один из трёх путей силы:')
-    print('Воитель, Маг, Лекарь')
-    char_class: str = choice_char_class()
-    print(start_training(char_name, char_class)) 
